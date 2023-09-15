3d7a98c95d0af59dfaa635ad1f4827906349458d
araz0515 <arazkubtanjian@gmail.com>
2023-09-15 02:51:11 -0400

    Added media query to hero

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Currency Converter</h1>
+        <h1 className="main-title">Currency Converter</h1>
         <h1>Real-Time, Intra-Day Data</h1>
         <h2>
           {" "}
           Delivering exchanging rates data for more than 170 world currencies
         </h2>
       </header>
+      <section></section>
     </div>
   );
 }
