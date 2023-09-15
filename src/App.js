b8677d827113adaf052c5d0afa9b7c27affeb9f8
araz0515 <arazkubtanjian@gmail.com>
2023-09-15 02:37:35 -0400

    Added landing page hero

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <header className="App-header">
+        <h1>Currency Converter</h1>
+        <h1>Real-Time, Intra-Day Data</h1>
+        <h2>
+          {" "}
+          Delivering exchanging rates data for more than 170 world currencies
+        </h2>
+      </header>
     </div>
   );
 }
